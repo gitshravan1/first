e40a326091682fddf34d1a05fb3862714d403cc8
unknown <shravantalokar2005@gmail.com>
2024-04-27 22:43:50 +0530

    modified after cloning

--- launch.java
+++ launch.java
@@ -4,5 +4,12 @@ public static void main(String[] args){
 system.out.println("Exploring Git and Github");
 
 }
+public static void fun(){
+     int a =10;
+int b=20;
+int sum =a+b;
+system.out.println(sum);
+
+}
 
 }
